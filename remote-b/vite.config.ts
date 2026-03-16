d574f6ddf682a801c4da15184a35d7be04ff7fdf
Nguyen Van Minh <nvminh162@gmail.com>
2026-03-16 20:39:19 +0700

    Add @originjs/vite-plugin-federation to enable module federation in remote components

--- remote-b/vite.config.ts
+++ remote-b/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  preview: {
+    port: 5005, //port you want to serve this remote
+  },
+  plugins: [
+    vue(),
+    federation({
+      name: "remote-a", //name of remote you want to use on host side
+      filename: "remote-b-source.js", //filename after the build
+      exposes: {
+        "./App": "./src/App.vue",  //target component you want to serve as remote side.
+        "./pages/Home": "./src/pages/Home.vue",  //target component you want to serve as remote side.
+      },
+      shared: ["vue"],  //we don't want to build our remote with a library the host side already have. So here we sinalize "hey, use this host side package"
+    }),
+    
+  ],
+  build: {
+    target: "es2022"
+  },
 })
